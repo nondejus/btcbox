413ebd1fd96221f979ad13a9eefd8478e84052a9
bitrinjani <bit.rinjani@gmail.com>
2018-02-18 10:51:32 +0900

    fix casing

--- __tests__/nocksetup.ts
+++ __tests__/nocksetup.ts
@@ -1,15 +1,15 @@
 // tslint:disable:max-line-length
 import * as nock from 'nock';
-import * as testdata from './testdata';
+import * as testData from './testData';
 
 export function nocksetup() {
   const api = nock('https://www.btcbox.co.jp');
-  api.get('/api/v1/depth/').reply(200, testdata.depth);
-  api.post('/api/v1/balance/').reply(200, testdata.balance);
-  api.post('/api/v1/balance/').reply(200, testdata.balance);
-  api.post('/api/v1/trade_cancel/').reply(200, testdata.cancel);
-  api.post('/api/v1/trade_add/').reply(200, testdata.send);
-  api.post('/api/v1/trade_view/').reply(200, testdata.getOrder);
-  api.post('/api/v1/trade_view/').reply(200, testdata.getOrder2);
-  api.post('/api/v1/trade_view/').reply(200, testdata.getOrder3);
+  api.get('/api/v1/depth/').reply(200, testData.depth);
+  api.post('/api/v1/balance/').reply(200, testData.balance);
+  api.post('/api/v1/balance/').reply(200, testData.balance);
+  api.post('/api/v1/trade_cancel/').reply(200, testData.cancel);
+  api.post('/api/v1/trade_add/').reply(200, testData.send);
+  api.post('/api/v1/trade_view/').reply(200, testData.getOrder);
+  api.post('/api/v1/trade_view/').reply(200, testData.getOrder2);
+  api.post('/api/v1/trade_view/').reply(200, testData.getOrder3);
 }
